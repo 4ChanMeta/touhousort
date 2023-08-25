a2220a56756a4dda80fa5f2477b0185cc1abb2ce
4ChanMeta <56356893+4ChanMeta@users.noreply.github.com>
2023-08-24 23:14:45 -0400

    Update 2023-05-09.js

--- src/js/data/2023-05-09.js
+++ src/js/data/2023-05-09.js
@@ -1,4 +1,4 @@
-dataSetVersion = "2023-06-09"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSetVersion = "2023-05-09"; // Change this when creating a new data set version. YYYY-MM-DD format.
 dataSet[dataSetVersion] = {};
 
 dataSet[dataSetVersion].options = [
